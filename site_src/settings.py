728317c83bacdbfa4cd144228734878a1fa835c2
nathius262 <egbodonathaniel@yahoo.com>
2023-12-04 21:15:01 +0100

    modified settings.py

--- site_src/settings.py
+++ site_src/settings.py
@@ -202,6 +202,8 @@ MEDIA_URL = '/media/'  # or any prefix you choose
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_cdn/')
+
 #STATIC_ROOT = os.path.join(BASE_DIR, '/static/')
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static/'),
