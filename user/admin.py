547b762331d3ebabb08d568b81f1ccbb7f362b60
nathius262 <egbodonathaniel@yahoo.com>
2023-12-04 13:59:06 +0100

    modified user app

--- user/admin.py
+++ user/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
-from .models import Profile
+from .models import Profile, UserTeam
 
 # Register your models here.
 class ProfileAdmin(admin.ModelAdmin):
     list_display = ['user',]
+    
+
+class UserTeamAdmin(admin.ModelAdmin):
+    list_display = ['user_id', 'team_id']
 
 
 admin.site.register(Profile, ProfileAdmin)
+admin.site.register(UserTeam, UserTeamAdmin)
--- user/models.py
+++ user/models.py
@@ -1,10 +1,19 @@
 from django.db import models
 from authentication.models import CustomUser
+from fantasy.models import Team
 
 # Create your models here.
 
 class Profile(models.Model):
-    user = models.ForeignKey(CustomUser, verbose_name="users", on_delete=models.CASCADE, related_name="user_profile")
+    user = models.OneToOneField(CustomUser, verbose_name="users", on_delete=models.CASCADE, related_name="user_profile")
     
     def __str__(self):
         return self.user.username
+    
+class UserTeam(models.Model):
+    user_id = models.ForeignKey(Profile, on_delete=models.CASCADE, null=True, blank=False)
+    team_id = models.ForeignKey(Team, on_delete=models.CASCADE, null=True, blank=False)
+    
+    def __str__(self):
+        return self.user_id
+    
